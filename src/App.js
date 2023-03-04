cd3e8a895720b886bed8ba4fd1acaf731cb86fc8
Veysel Aydin <weycell.aydin09@gmail.com>
2023-03-04 17:30:43 +0300

    data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import { useEffect, useState } from "react";
 import Header from "./components/Header";
 import HeaderMemo from "./components/HeaderMemo";
+function App() {
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState("");
+  const [search, setSearch] = useState("");
+  const [data, setData] = useState([]);
 
-const App = () => {
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setData(data);
+      });
+  }, []);
+
   return (
     <div className="container mt-2">
       <Header />
