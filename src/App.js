69711b9ab42c1c60ad251282bc3f3db4c2e4e26d
Veysel Aydin <weycell.aydin09@gmail.com>
2023-03-04 17:27:05 +0300

    starter

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Header from "./components/Header";
+import HeaderMemo from "./components/HeaderMemo";
+
+const App = () => {
+  return (
+    <div className="container mt-2">
+      <Header />
+
+      <HeaderMemo />
+    </div>
+  );
+};
+
+export default App;
