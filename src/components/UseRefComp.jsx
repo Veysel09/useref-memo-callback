fb0b6fe57e3e698996275db7af9b5a250278f427
Veysel Aydin <weycell.aydin09@gmail.com>
2023-03-05 11:58:38 +0300

    useRef

--- src/components/UseRefComp.jsx
+++ src/components/UseRefComp.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from "react";
 
 const UseRefComp = () => {
-    return (
-        <div>UseRefComp</div>
-    )
-}
+  const divRef = useRef();
+  const inputRef = useRef();
+  const [headerText, setHeaderText] = useState("useref");
+  const handleRef = () => {
+    console.log(divRef.current);
+    console.log(divRef.current.firstElementChild.innerText);
+    console.log(inputRef.current);
+    console.log(inputRef.current.value);
+    setHeaderText(inputRef.current.value);
+    inputRef.current.value = "";
+  };
 
-export default UseRefComp
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
+
+  return (
+    <div>
+      <h5>{headerText}</h5>
+      <div ref={divRef}>
+        <p>First Element Child</p>
+        <input type="text" ref={inputRef} />
+        <p>last Element Child</p>
+      </div>
+      <button onClick={handleRef}>useRef Click</button>
+    </div>
+  );
+};
+
+export default UseRefComp;
