618298eed95479625f8e7a028c8f5e33ded8b875
Veysel Aydin <weycell.aydin09@gmail.com>
2023-03-05 11:55:29 +0300

    appjs

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useMemo, useRef, useState } from "react";
 import Card from "./components/Card";
+import ClearButton from "./components/ClearButton";
 import Header from "./components/Header";
 import HeaderMemo from "./components/HeaderMemo";
+import TaxComp from "./components/TaxComp";
+import UseRefComp from "./components/UseRefComp";
+
 function App() {
   const [count, setCount] = useState(0);
   const [text, setText] = useState("");
   const [search, setSearch] = useState("");
   const [data, setData] = useState([]);
+
+  // const taxData = {"tax": 0.18, "ship": 15}
+  const taxData = useRef({ tax: 0.18, ship: 15 });
 
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/users")
@@ -17,14 +24,37 @@ function App() {
       });
   }, []);
 
+  // const filteredData = data?.filter(item =>item.name.toLowerCase().includes(search.toLowerCase()))
+
+  const filteredData = useMemo(() => {
+    return data?.filter((item) =>
+      item.name.toLowerCase().includes(search.toLowerCase())
+    );
+  }, [data, search]);
+
+  const handleSearch = () => {
+    setSearch(text);
+  };
+
+  // const handleClear = () =>{
+  //   setText("")
+  //   setSearch("")
+  // }
+
+  const handleClear = useCallback(() => {
+    setText("");
+    setSearch("");
+  }, []);
+
   return (
     <div className="container mt-2">
       <div>
         <Header />
         <hr />
-        <HeaderMemo />
+        <HeaderMemo count={count < 5 ? 0 : count} />
       </div>
       <hr />
+      <TaxComp taxData={taxData} />
       <div>
         <p>{count}</p>
         <button className="btn btn-danger" onClick={() => setCount(count + 1)}>
@@ -33,12 +63,28 @@ function App() {
       </div>
       <hr />
       <div style={{ display: "flex", justifyContent: "center" }}>
-        <input type="text" />
-        <button type="button">Search</button>
+        <input
+          type="text"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+        <button type="button" onClick={handleSearch}>
+          Search
+        </button>
       </div>
       <div className="row">
-        <Card data={data} />
+        <Card data={filteredData} />
       </div>
+      <hr />
+      <ClearButton handleClear={handleClear} />
+      <br />
+      <br />
+      <hr />
+      <h1>UseRef</h1>
+      <UseRefComp />
+      <br />
+      <br />
+      <br />
     </div>
   );
 }
